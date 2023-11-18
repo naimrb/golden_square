a4fe39dbf513a720a9c20bd6f735060f1b9fa54a
naimrb <naimbarka@hotmail.com>
2023-11-18 03:10:00 +0000

    Added phase two chapter 05

---
+++ phase_two/05/lib/diary_entry.py
@@ -0,0 +1,53 @@
+class DiaryEntry:
+    def __init__(self, title, contents):
+        # Parameters:
+        #   title: string
+        #   contents: string
+        self.title = title
+        self.contents = contents
+        self.placeholder = 0
+
+    def format(self):
+        # Returns:
+        #   A formatted diary entry, for example:
+        #   "My Title: These are the contents"
+        return self.title + ': ' + self.contents
+
+    def count_words(self):
+        # Returns:
+        #   int: the number of words in the diary entry
+        words = self.contents.split()
+        return len(words)
+    
+    def reading_time(self, wpm):
+        # Parameters:
+        #   wpm: an integer representing the number of words the user can read 
+        #        per minute
+        # Returns:
+        #   int: an estimate of the reading time in minutes for the contents at
+        #        the given wpm.
+        num_words = self.count_words()
+        return (num_words / wpm)
+
+    def reading_chunk(self, wpm, minutes):
+        # Parameters
+        #   wpm: an integer representing the number of words the user can read
+        #        per minute
+        #   minutes: an integer representing the number of minutes the user has
+        #            to read
+        # Returns:
+        #   string: a chunk of the contents that the user could read in the
+        #           given number of minutes
+        #
+        # If called again, `reading_chunk` should return the next chunk,
+        # skipping what has already been read, until the contents is fully read.
+        # The next call after that should restart from the beginning.
+        words = self.contents.split()
+        readable_words = wpm * minutes
+
+        if self.placeholder >= len(words):
+            self.placeholder = 0
+
+        result = ' '.join(words[self.placeholder:self.placeholder+readable_words])
+        self.placeholder += readable_words
+        return result
