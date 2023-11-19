5bac211bab70c4cb739e6a486c1ae11c339b50c7
naimrb <naimbarka@hotmail.com>
2023-11-19 22:01:16 +0000

    Added phase two chapter 08

---
+++ phase_two/08_test_drive_a_class_system/lib/diary.py
@@ -0,0 +1,60 @@
+class Diary:
+    def __init__(self):
+        self.entries = []
+
+    def add(self, entry):
+        # Parameters:
+        #   entry: an instance of DiaryEntry
+        # Returns:
+        #   Nothing
+        # Side-effects:
+        #   Adds the entry to the entries list
+        self.entries.append(entry)
+
+    def all(self):
+        # Returns:
+        #   A list of instances of DiaryEntry
+        return self.entries
+
+    def count_words(self):
+        # Returns:
+        #   An integer representing the number of words in all diary entries
+        # HINT:
+        #   This method should make use of the `count_words` method on DiaryEntry.
+        num_words = 0
+
+        for entry in self.entries:
+            num_words += entry.count_words()
+        
+        return num_words
+
+    def reading_time(self, wpm):
+        # Parameters:
+        #   wpm: an integer representing the number of words the user can read
+        #        per minute
+        # Returns:
+        #   An integer representing an estimate of the reading time in minutes
+        #   if the user were to read all entries in the diary.
+        num_words = self.count_words()
+        return int(num_words / wpm)
+
+    def find_best_entry_for_reading_time(self, wpm, minutes):
+        # Parameters:
+        #   wpm:     an integer representing the number of words the user can
+        #            read per minute
+        #   minutes: an integer representing the number of minutes the user has
+        #            to read
+        # Returns:
+        #   An instance of DiaryEntry representing the entry that is closest to,
+        #   but not over, the length that the user could read in the minutes
+        #   they have available given their reading speed.
+        readable_words = wpm * minutes
+        best = None
+        num_words_closest = 0
+
+        for entry in self.entries:
+            if entry.count_words() <= readable_words and entry.count_words() > num_words_closest:
+                best = entry
+                num_words_closest = entry.count_words()
+        
+        return best
